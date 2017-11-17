b1661bd7b9bfe007197c97ce7717b3c12e047e61
1 <1>
2017-11-17 10:26:09 +0800

    1

---
+++ server/release/start-debug.bat
@@ -0,0 +1,2 @@
+java -Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n -jar ServerProject-0.0.1-SNAPSHOT.jar
+pause
---
+++ server/release/start.bat
@@ -0,0 +1,2 @@
+java -jar ServerProject-0.0.1-SNAPSHOT.jar
+pause
